0c370e64a8ca843420ae4bd8ba2ec736103cdc00
Sarvesh Patil <sarvesh.patil@plivo.com>
2026-07-17 11:09:40 +0530

    Add Plivo integration package (integrations/plivo/src/index.ts)

---
+++ integrations/plivo/src/index.ts
@@ -0,0 +1,6 @@
+export { getPlivoClient, type PlivoClientOptions, type PlivoClient } from './client.js';
+export { createPlivoSendSmsTool } from './send-sms.js';
+export { createPlivoMakeCallTool } from './make-call.js';
+export { createPlivoTools } from './tools.js';
+export { plivoInboundSmsRoute } from './routes.js';
+export type { PlivoInboundSms, PlivoInboundSmsRouteArgs, PlivoInboundSmsRouteOptions } from './routes.js';
